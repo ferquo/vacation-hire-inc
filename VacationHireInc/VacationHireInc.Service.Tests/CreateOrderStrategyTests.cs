05b1169557cdbcfce4614baaf702cf557b1991e4
ferquo <ferquo07@gmail.com>
2023-05-30 19:00:05 +0300

    Added a unit test

---
+++ VacationHireInc/VacationHireInc.Service.Tests/CreateOrderStrategyTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AutoMapper;
+using FluentAssertions;
+using Moq;
+using VacationHireInc.DataAccess.Repositories.Interfaces;
+using VacationHireInc.Domain.DataTransferObjects;
+using VacationHireInc.Service.Orders;
+using VacationHireInc.Service.Orders.Validators;
+
+namespace VacationHireInc.Service.Tests
+{
+	public class CreateOrderStrategyTests
+	{
+        [Theory]
+        [InlineData("")]
+        [InlineData("1")]
+        [InlineData("22")]
+        public async Task CreatingOrderWithInvalidCustomerNAmeWillThrowExcpetion(string customerName)
+        {
+            // arrange
+            var order = new OrderForCreationDto
+            {
+                CustomerName = customerName
+            };
+            var config = new MapperConfiguration(c => {
+                c.AddProfile<MappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+            var repository = new Mock<IOrdersRepository>();
+            var validator = new OrderCreationValidator(new Mock<IRentableProductRepository>().Object);
+
+            var subject = new CreateOrderStrategy(repository.Object, validator, mapper);
+
+            // act
+            Func<Task> act = () => subject.CreateOrder(order);
+
+            // assert
+            await act.Should().ThrowAsync<FluentValidation.ValidationException>();
+        }
+    }
+}
+
